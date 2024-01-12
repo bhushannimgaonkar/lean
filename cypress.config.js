d7f56a2e20e3672b096c1032f4341b3f96f5050d
bhushannimgaonkar <bhushan.nimgaonkar@gmail.com>
2024-01-12 14:44:21 +1000

    committed login function test case

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: '3q31f4',
   e2e: {
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/loginFunction.cy.js
@@ -0,0 +1,54 @@
+describe("verify login function of the project",()=>{
+it('with valid login and valid password',()=>{
+     
+    cy.visit('/web/index.php/auth/login')
+
+    cy.get('input[name="username"]').type("Admin")
+     
+    cy.get('input[name="password"]').type("admin123")
+
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('Dashboard').should('be.visible')
+})
+
+it('with invalid login and valid password',()=>{
+     
+    cy.visit('/web/index.php/auth/login')
+
+    cy.get('input[name="username"]').type("sample")
+     
+    cy.get('input[name="password"]').type("admin123")
+
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('Invalid credentials').should('be.visible')
+})
+
+it('with valid login and invalid password',()=>{
+     
+    cy.visit('/web/index.php/auth/login')
+
+    cy.get('input[name="username"]').type("Admin")
+     
+    cy.get('input[name="password"]').type("sample")
+
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('Invalid credentials').should('be.visible')
+})
+it('with invalid login and invalid password',()=>{
+     
+    cy.visit('/web/index.php/auth/login')
+
+    cy.get('input[name="username"]').type("sample")
+     
+    cy.get('input[name="password"]').type("sample")
+
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('Invalid credentials').should('be.visible')
+})
+
+
+})
